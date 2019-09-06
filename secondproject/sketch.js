0a308a56334beba89ff3206f5873d0e632b0b1c7
SaskatoonBoy <51387702+SaskatoonBoy@users.noreply.github.com>
2019-09-06 13:42:14 -0600

    Update/Second Project Files

---
+++ secondproject/sketch.js
@@ -0,0 +1,17 @@
+// First Project
+// Eric James
+// Sept. Thrus. 5th 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  fill(0);
+  ellipse(mouseX, mouseY, 100, 100);
+}

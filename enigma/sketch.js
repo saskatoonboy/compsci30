621b3d7f3b429ae3a5e3a07c70e02e700817e719
SaskatoonBoy <51387702+SaskatoonBoy@users.noreply.github.com>
2019-09-12 12:23:10 -0600

    Update sketch.js

--- enigma/sketch.js
+++ enigma/sketch.js
@@ -5,10 +5,30 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let displayWindow = "lightBoard";
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(0, 100, 200);
+  background(220);
+  textAlign(CENTER);
+  if (displayWindow === "lightBoard") {
+    text("Light Board", width/2, height/2);
+    rect(width/2-50, height-20, 100, 20);
+    rect(width/2+50, height-20, 100, 20);
+  } else if (displayWindow === "plugBoard") {
+    text("Plug Board", width/2, height/2);
+    rect(width/2-50, height-20, 100, 20);
+    rect(width/2+50, height-20, 100, 20);
+  } else if (displayWindow === "wheels") {
+    text("Wheels", width/2, height/2);
+    rect(width/2-50, height-20, 100, 20);
+    rect(width/2+50, height-20, 100, 20);
+  } 
+}
+
+function drawButtons() {
+  
 }

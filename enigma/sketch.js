f01874ed56dcca7a50b9dc2b6c447daeef233297
SaskatoonBoy <51387702+SaskatoonBoy@users.noreply.github.com>
2019-09-19 14:16:03 -0600

    Update sketch.js

--- enigma/sketch.js
+++ enigma/sketch.js
@@ -10,9 +10,16 @@ let lampOffColour = [220, 220, 220]; // the colour of a lamp that is off
 let lampOnColour = [240, 250, 0]; // the colour of a lamp that is on
 let onLamp = ""; // the lamp that is on
 
-let rotors = ["EKMFLGDQVZNTOWYHXUSPAIBRCJEKMFLGDQVZNTOWYHXUSPAIBRCJEKMFLGDQVZNTOWYHXUSPAIBRCJ", "AJDKSIRUXBLHWTMCQGZNPYFVOEAJDKSIRUXBLHWTMCQGZNPYFVOEAJDKSIRUXBLHWTMCQGZNPYFVOE" ,"BDFHJLCPRTXVZNYEIWGAKMUSQOBDFHJLCPRTXVZNYEIWGAKMUSQOBDFHJLCPRTXVZNYEIWGAKMUSQO" ,"ESOVPZJAYQUIRHXLNFTGKDCMWBESOVPZJAYQUIRHXLNFTGKDCMWBESOVPZJAYQUIRHXLNFTGKDCMWB" ,"VZBRGITYUPSDNHLXAWMJQOFECKVZBRGITYUPSDNHLXAWMJQOFECKVZBRGITYUPSDNHLXAWMJQOFECK" ,"JPGVOUMFYQBENHZRDKASXLICTWJPGVOUMFYQBENHZRDKASXLICTWJPGVOUMFYQBENHZRDKASXLICTW" ,"NZJHGRCXMYSWBOUFAIVLPEKQDTNZJHGRCXMYSWBOUFAIVLPEKQDTNZJHGRCXMYSWBOUFAIVLPEKQDT" ,"FKQHTLXOCBJSPDZRAMEWNIUYGVFKQHTLXOCBJSPDZRAMEWNIUYGVFKQHTLXOCBJSPDZRAMEWNIUYGV"];
+let rotors = [[4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9], 
+  [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4], 
+  [1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14],
+  [4, 18, 14, 21, 15, 25, 9, 0, 24, 16, 20, 8, 17, 7, 23, 11, 13, 5, 19, 6, 10, 3, 2, 12, 22, 1],
+  [21, 25, 1, 17, 6, 8, 19, 24, 20, 15, 18, 3, 13, 7, 11, 23, 0, 22, 12, 9, 16, 14, 5, 4, 2, 10],
+  [9, 15, 6, 21, 14, 20, 12, 5, 24, 16, 1, 4, 13, 7, 25, 17, 3, 10, 0, 18, 23, 11, 8, 2, 19, 22],
+  [13, 25, 9, 7, 6, 17, 2, 23, 12, 24, 18, 22, 1, 14, 20, 5, 0, 8, 21, 11, 15, 4, 10, 16, 3, 19],
+  [5, 10, 16, 7, 19, 11, 23, 14, 2, 1, 9, 18, 15, 3, 25, 17, 0, 12, 4, 22, 13, 8, 20, 24, 6, 21]];
 let currentRotors = [0, 1, 2];
-let rotorOffset = [26, 26, 26];
+let rotorOffset = [0, 0, 0];
 
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
@@ -141,31 +148,25 @@ function drawRotors() {
 
   }
 
-  // first rotor letters
-
-  text(rotors[currentRotors[0]][rotorOffset[0]+1], width/6-25, 135);
-  text(rotors[currentRotors[0]][rotorOffset[0]], width/6-20, 185);
-  text(rotors[currentRotors[0]][rotorOffset[0]-1], width/6-25, 235);
-
-  // second rotor letters
-
-  text(rotors[currentRotors[1]][rotorOffset[1]+1], width/2-25, 135);
-  text(rotors[currentRotors[1]][rotorOffset[1]], width/2-20, 185);
-  text(rotors[currentRotors[1]][rotorOffset[1]-1], width/2-25, 235);
-
-  // third rotor letters
-
-  text(rotors[currentRotors[2]][rotorOffset[2]+1], width - width/6-25, 135);
-  text(rotors[currentRotors[2]][rotorOffset[2]], width - width/6-20, 185);
-  text(rotors[currentRotors[2]][rotorOffset[2]-1], width - width/6-25, 235);
-
+  // rotor letters
+  for (let rotor = 0; rotor < 3; rotor ++) {
+    for (let offset = 1; offset >= -1; offset --) {
+      let totalOffset = rotorOffset[rotor]+offset;
+      if (totalOffset < 0) {
+        totalOffset += 26;
+      } else if (totalOffset > 25) {
+        totalOffset -= 26;
+      }
+      text(alphabet[rotors[currentRotors[rotor]][totalOffset]], width/6-25+width/3*rotor, 185 - offset * 50);
+    }
+  }
 }
 
 function moveRotor(rotor, amount) {
   rotorOffset[rotor-1] += amount;
-  if (rotorOffset[rotor-1] < 26) {
+  if (rotorOffset[rotor-1] < 0) {
     rotorOffset[rotor-1] += 26;
-  } else if (rotorOffset[rotor-1] > 51) {
+  } else if (rotorOffset[rotor-1] > 25) {
     rotorOffset[rotor-1] -= 26;
   }
 }
@@ -194,9 +195,23 @@ function getRotorEncryption(rotor, forward, char) {
   let rotorStrip = rotors[currentRotors[rotor-1]];
 
   if (forward) {
-    return (rotorStrip[alphabet.indexOf(char)+rotorOffset[rotor-1]]);
+    let rotorStripIndex = char+rotorOffset[rotor-1];
+    print(rotorStripIndex);
+    if (rotorStripIndex < 0) {
+      rotorStripIndex += 26;
+    } else if (rotorStripIndex > 25) {
+      rotorStripIndex -= 26;
+    }
+    print(rotorStripIndex);
+    print(rotorStrip[rotorStripIndex]);
+    return (rotorStrip[rotorStripIndex]);
   } else {
-    return (alphabet[rotorStrip.indexOf(char)+rotorOffset[rotor-1]]);
+    rotorStripIndex = rotorStrip.indexOf(char)+26;
+    print(rotorStripIndex);
+    rotorStripIndex -= rotorOffset[rotor-1];
+    print(rotorStripIndex);
+    print(rotorOffset[rotor-1]);
+    return (alphabet[rotorStripIndex]);
   }
 
 }
@@ -204,10 +219,10 @@ function getRotorEncryption(rotor, forward, char) {
 
 // reflector
 
-let reflector = "YRUHQSLDPXNGOKMIEBFZCWVJATYRUHQSLDPXNGOKMIEBFZCWVJATYRUHQSLDPXNGOKMIEBFZCWVJAT";
+let reflector = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19];
 
 function getReflected(char) {
-  return (reflector[alphabet.indexOf(char)]);
+  return (reflector[char]);
 }
 
 // window buttons
@@ -264,7 +279,7 @@ function keyPressed() {
   if (displayWindow === "lightBoard") {
     moveRotors();
     print("Key pressed: " + key)
-    let letter = getRotorEncryption(1, true, key.toUpperCase());
+    let letter = getRotorEncryption(1, true, alphabet.indexOf(key.toUpperCase()));
     print("First Rotor: " + letter)
     letter = getRotorEncryption(2, true, letter);
     print("Second Rotor: " + letter)

bfdc522308d9b447f16bef3bba26738f3f32b765
SaskatoonBoy <51387702+SaskatoonBoy@users.noreply.github.com>
2019-09-25 14:11:18 -0600

    design case spacing

--- french/sketch.js
+++ french/sketch.js
@@ -6,6 +6,9 @@
 // - describe what you did to take this project "above and beyond"
 
 let binaryImg;
+let drawCases = [[case00], [case10]]
+let page = 0;
+let caseCount = [5, 5];
 
 function preload() {
   binaryImg = loadImage('assets/binary.jfif');
@@ -18,7 +21,29 @@ function setup() {
 
 function draw() {
   fill(220);
-  for (let cases = 0; cases < 12; cases ++) {
-    rect(300*cases/2, 200*cases%2, 200, 100);
+  for (let cases = 0; cases < caseCount[page]; cases ++) {
+    rect(width/3*(cases-cases%2)/2+5, height/2*(cases%2)+15, width/3-10, height/2-30);
+    if (drawCases[page].length > cases) {
+      drawCases[page][cases]();
+    }
   }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function person(x, y) {
+
+  ellipse(x, y, 100, 200)
+
+}
+
+function case00() {
+  fill(255);
+  person(100, 100);
+}
+
+function case10() {
+  circle(100, 100, 100, 100);
 }

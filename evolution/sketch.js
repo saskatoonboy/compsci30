268c710a265d34414ee5582e7ab8000af8dcaa34
saskatoonboy <54918014+saskatoonboy@users.noreply.github.com>
2019-10-07 12:26:21 -0600

    Update sketch.js

--- evolution/sketch.js
+++ evolution/sketch.js
@@ -1,85 +1,117 @@
-// Line Art
+// Evolution Emulator
 // Eric James
-// Sept. Mon. 9th 2019
+// Oct. Wed. 2nd 2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let binaryImg;
-let drawCases = [[],[],[]]
-let page = 0;
-let caseCount = [5, 5];
-let ellipses = [];
-let control = "1";
-
-function preload() {
-  binaryImg = loadImage('assets/binary.jfif');
-}
+const meter = 250;
+const nodeSize = 25;
+const MuscleWidth = 10;
+let timeMultiplier = 1;
+let groundY;
+let display = -1;
+let creatures = [];
+let creaturePos = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
+  for (let i = 0; i < 1000; i++) {
+    creatures[i] = new Creature();
+  }
+  groundY = height - 200;
 }
 
 function draw() {
-  for (let cases = 0; cases < caseCount[page]; cases ++) {
-    fill(220);
-    rect(width/3*(cases-cases%2)/2+5, height/2*(cases%2)+15, width/3-10, height/2-30);
-    if (drawCases[page].length > cases) {
-      drawCases[page][cases]();
+  drawBackground();
+
+  if (display > -1) {
+    const creature = creatures[display];
+
+    if (!creature.running) {
+      creature.run();
+    }
+
+    for (let t = 0; t < timeMultiplier; t++) {
+      if (!creature.update()) {
+        display++;
+        if (display > 999) {
+          display = -1;
+        }
+      }
+    }
+    creature.draw();
+    creature.displayStats();
+  } else if (display === -1) {
+    text("Multi: " + timeMultiplier, width - 200, 200);
+  } else if (display === -2) {
+
+    creaturePos[0] = creatures[0].x;
+
+    if (creatures[1].x > creaturePos[0]) {
+      creaturePos[1] = creaturePos[0];
+      creaturePos[0] = creatures[1].x;
+    }
+
+    for (let creatureIndex = 2; creatureIndex < creatures.length; creatureIndex++) {
+      let shiftIndex = -1;
+      let creaturePosIndex = 0
+      while (shiftIndex === -1) {
+        if (creatures[creatureIndex].x > creaturePos[creaturePosIndex]) {
+          shiftIndex = creaturePosIndex;
+        }
+        if (creaturePosIndex === creaturePos.length - 1) {
+          shiftIndex = creaturePos.length;
+        }
+        creaturePosIndex++;
+      }
+      creaturePos[creatureIndex] = creatures[creatureIndex].x;
     }
   }
+}
 
-  fill(255);
-  drawEllipses();
-
+function drawBackground() {
+  if (display > -1) {
+    background(100, 100, 255);
+    noStroke();
+    fill(8, 133, 14);
+    rect(0, groundY, width, 200);
+  } else {
+    background(255, 166, 0);
+  }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-}
-
-function drawEllipses() {
-  for (let ellipseIndex = 0; ellipseIndex < ellipses.length; ellipseIndex ++) {
-    ellipseMode(CORNER);
-    angleMode(DEGREES);
-    rotate(ellipses[ellipseIndex][4]);
-    fill(ellipses[ellipseIndex][5][0]);
-    ellipse(ellipses[ellipseIndex][0], ellipses[ellipseIndex][1], ellipses[ellipseIndex][2], ellipses[ellipseIndex][3]);
-    rotate(360-ellipses[ellipseIndex][4]);
-  }
-}
-
-function person() {
-
-  ellipses.push([mouseX, mouseY, 50, 75, 0, [0, 0, 0]]);
-  ellipses.push([mouseX, mouseY, 50, 75, 90, [50, 50, 50]]);
-  ellipses.push([mouseX, mouseY, 50, 75, 180, [100, 100, 100]]);
-  ellipses.push([mouseX, mouseY, 50, 75, 270, [150, 150 , 150]]);
-
-}
-
-function addEllipse(x, y, w, h, r, c) {
-  ellipses.push([x, y, w, h, r, c]);
-}
-
-function mouseClicked() {
-  if (control === "1") {
-
-    person()
-
-  } else if (control === "2") {
-
-    addEllipse(mouseX, mouseY, 50, 75, 0, [255, 255, 255]);
-
-  }
+  groundY = height - 200
 }
 
 function keyPressed() {
-  let options = ["1", "2"];
-  if (options.indexOf(key) > -1) {
+  if (key === "a") {
+    display--;
+  } else if (key === "d") {
+    display++;
+  } else if (key === "w") {
+    timeMultiplier++;
+  } else if (key === "s") {
+    timeMultiplier--;
+  }
 
-    control = key;
+  if (display < -1) {
+    display = 999;
+  } else if (display > 999) {
+    display = -2;
+  }
 
+  if (timeMultiplier < 1) {
+    timeMultiplier = 1;
   }
 }
+
+  // working creature sketch
+  //nodes[0] = new Node(width/2+50, height/2+100, 1);
+  //nodes[1] = new Node(width/2-50, height/2+100, 0);
+  //nodes[2] = new Node(width/2, height/2, 0.2);
+  //muscles[0] = new Muscle(nodes[0], nodes[1], 0.2, 1, 2.5, 300, 500);
+  //muscles[1] = new Muscle(nodes[1], nodes[2], 0.2, 0.5, 5, 500, 300);
+  //muscles[2] = new Muscle(nodes[0], nodes[2], 0.2, 0.5, 5, 100, 500);

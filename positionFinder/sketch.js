500957c81bd81e403d8da68a8659b4fd2846b64b
SaskatoonBoy <51387702+SaskatoonBoy@users.noreply.github.com>
2019-10-16 14:03:38 -0600

    started pong

--- positionFinder/sketch.js
+++ positionFinder/sketch.js
@@ -5,56 +5,57 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let classifier;
+let predictor;
 let mobilenet;
 let value = 0;
 let video;
 let slider;
 let addButton;
 let trainButton;
-let fishImg;
+let pastValues = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function modelReady() {
   console.log("Model Ready!");
-  //mobilenet.predict(gotResults);
+  predictor.load("assets/model.json", customModelReady);
+}
+
+function customModelReady() {
+  console.log("Custom Model Ready!");
+  predictor.predict(gotResults);
 }
 
 function videoReady() {
   console.log("Video Ready!");
 }
 
-function getLoss(loss) {
-  console.log(loss);
-  if (loss === null) {
-    classifier.classify(gotResults);
-  }
-}
+// function getLoss(loss) {
+//   console.log(loss);
+//   if (loss === null) {
+//     predictor.predict(gotResults);
+//     predictor.save();
+//   }
+// }
 
 function setup() {
-  createCanvas(windowWidth/2, windowHeight/2+50);
-
-  // fishImg = createImg("assets/fish.jpeg");
-  // fishImg.hide();
+  createCanvas(windowWidth, windowHeight);
 
   video = createCapture(VIDEO);
   video.hide();
 
   mobilenet = ml5.featureExtractor("MobileNet", modelReady);
-  classifier = mobilenet.classification(video, videoReady);
+  predictor = mobilenet.regression(video, videoReady);
 
   // slider = createSlider(0, 1, 0.5, 0.01);
 
-  addButton = createButton("Add Example");
-  addButton.mousePressed(function() {
-    classifier.addImage("phone")
-  });
+  // addButton = createButton("Add Example");
+  // addButton.mousePressed(function() {
+  //   predictor.addImage(slider.value());
+  // });
 
-  trainButton = createButton("Train");
-  trainButton.mousePressed(function() {
-    classifier.train(getLoss);
-  });
-
-  // classifier.classify(video, gotResults);
+  // trainButton = createButton("Train");
+  // trainButton.mousePressed(function() {
+  //   predictor.train(getLoss);
+  // });
 }
 
 function draw() {
@@ -63,12 +64,26 @@ function draw() {
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth/2, windowHeight/2+50);
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function displayVideo() {
-  image(video, 0, 0, width, height-50);
+  image(video, width/2, height/2-50, width/2, height-height/2+50);
+
+  let paddleY = value;
+
+  for (let thisValue of pastValues) {
+    paddleY += thisValue;
+  }
+  paddleY = paddleY / 10;
+  pastValues.shift();
+  pastValues.push(value);
+
   fill(255);
+  rectMode(CENTER);
+  rect(100, height*paddleY, 25, 100);
+
+  //fill(255);
   textSize(24);
   text(value, 0, height);
 }
@@ -77,8 +92,8 @@ function gotResults(error, result) {
   if (error) {
     console.log(error);
   } else {
-    // console.log(result);
-    value = result[0].label;
+    //console.log(result);
+    value = 1-result.value;
   }
-  classifier.classify(gotResults);
+  predictor.predict(gotResults);
 }
